49331afc8a99cd92668604eb6bbc0bb60b8a1061
Cedar <alexianfireflies@gmail.com>
2019-08-08 02:33:13 -0700

    added fonts, updated html, js, and css files

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# word-guess-game
+# Word Guess Game (Hangman)
+
+This is a prehistoric (mostly dinosaur)-themed hangman game. You hit any key to being the game, at which point the game selects a random word, displays on screen the same number of dashes as there are letters in the word, and then lets you enter guesses until you either run out of guesses and lose or guess the word correctly. If you guess a letter correctly, the display fills in the appropriate letter in the appropriate spots. You get six guesses, because in traditional hangman, there's a guess for the head, body, both arms, and both legs. The game also keeps track of letters you've already guessed and displays them on screen for you to see, and also will not take a guess away if you guess the same letter multiple times.
+If you win, you get an alert telling you that you've won and also what the word was; if you lose, you get an alert telling you that you've lost and also what the word was.
+
+## Installation
+
+You can use Git Bash to clone the repository onto your computer by entering the following in Git Bash:
+```git clone https://github.com/CedarHaven/word-guess-game.git```
+After doing so, open the folder you've downloaded, right click 'index.html' and open in your browser of choice.
+
+Alternately, the project is hosted on Git Pages at (link)
+
+## Acknowledgements
+CSS reset from here: https://meyerweb.com/eric/tools/css/reset/
+Burlesque font (used for the first header) originally from here: https://www.1001fonts.com/burlesque-font.html; inclusion of the font was made possible by the Webfont Generator here: https://www.fontsquirrel.com/tools/webfont-generator
+Skranji font (used for the second header) from Google Fonts.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,134 @@
 // the game is dinosaur themed. kind of. 
 // I guess it's more prehistoric-themed, actually.
-// it started dinosaur themed, but dinosaur names are like...ridiculously long, most of the time? and I needed at least a FEW shorter words. so I added things like 'raptor' and 'egg' and 'fossil' and then 'amber'...and then I added megalodon, because who doesn't love a giant shark?
+// it started dinosaur themed, but dinosaur names are like...ridiculously long, most of the time? and I needed at least a FEW shorter words. so I added things like 'raptor' and 'egg' and 'fossil' and then 'amber'...and then I added megalodon, because amber isn't remotely dinosaur related and who DOESN'T love a giant shark?
+// anyway, this is the array of possible words you could have to guess.
 var words = ["fossil", "pterodactyl", "jurassic", "triassic", "mesozoic", "cretaceous", "triceratops", "velociraptor", "parasaurolophus", "ichthyosaur", "pliosaur", "mosasaur", "liopleurodon", "minmi", "raptor", "egg", "amber", "megalodon", "trilobite"];
 
-// you get six guesses (lives? chances to be wrong?), because the way I usually played hangman is that for every wrong guess, you drew on 1. a head, 2. a body, 3. an arm, 4. the other arm, 5. a leg, and 6. the other leg. so. 6 guesses.
-// is this unfair, given how long most of these words are? perhaps so. but oh well.
 var wins = 0;
 var losses = 0;
-var guesses = 6;
 
-// an array. every time the user guesses a letter, the guess will be appended to the array...and when you guess again, it will check the array to see if you've already guessed that letter. I hope. I'm not sure what will happen if I attempt to cycle through an empty array.
-var usedGuesses = [""];
+// an array. every time the user guesses a letter, I will check if the guess is already in the array. if it's not, it will be added to the array and then I'll check if your guess is in the string (and if it is, the word fills in on screen, and if it's not, you lose a guess). if it is, then I skip right over checking for the letter in the word and you don't lose a guess.
+// this forever causes the array to start with an empty space when it cycles through, but I'm not sure how else to make it work.
+var guessedLetters = [""];
 
-/* example of what I'm trying to do and some thoughts on the rest of the game stuff.
-// this forever causes the array to start with an empty space when it cycles through, but I'm not sure how else to make what I've done work.
-var empty = [""];
-var check = false;
-var guess = 6;
+// declared secretString and displayString. not sure I 100% had to do this here, but it's 1am on Thursday and I still have CSS and a readme to do. I'll try to come back to this later to test.
+var secretString = "";
+var displayString = "";
 
-document.onkeyup=function(event) {
-    var keyEnter = event.key.toLowerCase();
+// this is so we'll only start when the HTML is ready to go, because...safety! I guess!
+$(document).ready(function(){
 
-    for(i = 0; i < empty.length; i++){
-        if(empty[i] == keyEnter){
-            alert("wow, how?");
-            console.log(empty[i]);
-            check = false; // false, because we don't need to append the key pressed to the array--it's already in there.
-        }
-        else{
-            alert("does this work?");
-            console.log(empty[i]);
-            check = true; // true, because we DO need to append it to the array--it's not in there yet.
+    // the only way I could get the game to be consistently replayable was to make a display function that printed out things like the dashes for the string you'd be guessing and reset the 'guessedLetters' array to "" and 'guesses' to 6, then have the rest of the game in a function INSIDE display().
+    document.onkeyup = function() {display()};
+
+    function display(){
+        // here we reset guessedLetters to empty, set the secretString to a new word, set helper to be the same as secretString, and populate displayString.
+        guessedLetters = [""];
+        secretString = words[Math.floor(Math.random() * words.length)];
+        var helper = secretString;
+        displayString = populate(secretString.length);
+
+        // you get six guesses (lives? chances to be wrong?), because the way I usually played hangman is that for every wrong guess, you drew on 1. a head, 2. a body, 3. an arm, 4. the other arm, 5. a leg, and 6. the other leg. so. 6 guesses.
+        // is this unfair, given some of these words? perhaps so. but that's standard hangman!
+        // guesses resets to 6 whenever display() is called.
+        var guesses = 6;
+
+        // this displays the wins and losses, dashes, and the starting guess number in the HTML.
+        displayScores(wins, losses);
+        displayUnknownWord(displayString);
+        displayGuesses(guesses);
+
+        // this gets the div in the HTML that's meant to hold the letters you've guessed, and then also clears it out, in case this is a new game.
+        var userGuessDiv = document.getElementById("guesses");
+        userGuessDiv.innerHTML = "";
+
+        game();
+
+        function game(){
+            document.onkeyup = function(event){
+                var char = event.key.toLowerCase();
+    
+                console.log(char);
+    
+                // this checks if you've already guessed a letter or not; if it doesn't find the letter you enter in the array, it adds it to the array of guessed letters and carries on to see if the letter is in the word being guessed or not.
+                if(!guessedLetters.includes(char)) {
+                    guessedLetters.push(char);
+
+                    // it also appends the guess to the div made in the HTML for keeping list of what you've guessed.
+                    var insertGuess = document.createElement("span");
+                    insertGuess.textContent = char;
+                    userGuessDiv.appendChild(insertGuess);
+                        
+                    // if your guess is included in the word, then it displays the letter in all the places it's found. if it's NOT in the word, you lose a guess.
+                    if(secretString.includes(char)) {
+                        while(helper.includes(char)){
+                            var index = helper.indexOf(char);
+                            helper = helper.replace(char, "-");
+                            displayString = replaceAtChar(displayString, index, char);
+
+                            // calling game again inside of it was the only way I could get it to repeatedly let people enter letters. while loops did not work for this. at all.
+                            game();
+                        }
+                            
+                        displayUnknownWord(displayString);
+                    }
+                    else {
+                        guesses--;
+                        displayGuesses(guesses);
+                        game();
+                    }
+                }
+                else{
+                    // this is here so that in case you forget what you've already guessed and guess a letter you've already tried, game will keep going.
+                    game();
+                }
             }
+    
+            // in here I called back up to display so that whether you won or lost, the game would play again. I went with <= 0 because I was having a bug for a bit where sometimes guesses would increment to -1, and I couldn't figure out why, so I just...did this. just in case.
+            // I did test this with both a win and a loss. it seemed to work fine.
+            if(guesses <= 0) {
+                losses++;
+                displayScores(wins, losses);
+                alert("Sorry, you lost. The word was "+secretString+".");
+                display();
+            }
+            else if(displayString == secretString) {
+                wins++;
+                displayScores(wins, losses);
+                // for some reason, having the alert prevents the word from displaying fully on the screen, and I'm not sure how to fix that, but I really want the alert so that you actually, you know, know you won and can see the word before display() gets called again.
+                alert("You won! The word was "+secretString+". Congratulations on correctly guessing it!");
+                display();
+            }
+        }
+    }
+});
+
+function populate(length) {
+    var display = "";
+
+    for(i = 0; i < length; i++) {
+        display += "-";
     }
 
-    // if we didn't find the guess in the array--append.
-    if(check){
-        empty.push(keyEnter);
-    }
-    // if we didn't find it in the array, do nothing, because it's already in there (so you already tried it), which means regardless of if it's in the word or not, you don't lose a guess.
+    return display;
+}
 
-    // we return check to false. I'm not sure if this is needed but I'm very brain foggy right now and I'm putting it in anyway.
-    check = false;
+function displayUnknownWord(displayString) {
+    document.getElementById("word-game").innerHTML = displayString;
+}
 
-    // I am also going to have to then have an array run through the string, and check if the letter guessed (as long as it wasn't found in the array already) is in the word. and if it's not in the word, you lose a guess. if it is in the word, we fill in the spaces where the letter appears.
-    // and I am going to have to create a section for the letters guessed, and when a guess is entered that ISN'T found in the array appears, we append that guessed letter to the <div> for the guesses. this should happen in the same section where the letter is appended to the array of guesses.
-    // fuck. I've just realized: I need this to be a global variable, because it can't be getting re-declared every single time there's a keypress, but that means that at the end of the game the array will be full of guesses--AH. make a function that, on win or loss, clears the array back to just "". so...I'll need a win condition and then at the end of each keypress, I'll have to check if the win condition or the loss condition are true, and if either is true, reset the array to "", and if neither is true, leave the array alone.
-    // and I just realized the game is supposed to start generating on a keypress. FUCK. I don't know. I'm lost and confused and everything sucks today, because I HAVE to use the keypress to append to the array of guesses and check....maybe I should have an initial keypress, and then after that every keypress registers with the stuff that appends to the array and things? can I do that?? can I run a function only if the game has started?
-    // https://stackoverflow.com/questions/28069638/check-if-function-returns-true-to-execute-another-function/28069688 ?
-    // have the initial document.onkeypress start the game, by setting a boolean to true and returning its value (global boolean? gameStart = false to begin with?), and then once the boolean is true, jump to running a game function, which will then select a word from the array, print out the empty spaces for the word, start logging user guesses in the array and all, and start counting guesses and wins and losses and checking for if the user guss is in the word, and if the user runs out of guesses they lose (losecondition true) and if they get all the letters in the word they win (wincondtion true), and then when win or lose is true, the game...resets?
-    // YES! the game resets, using a 'return', like so: https://stackoverflow.com/questions/2940862/javascript-how-to-restart-a-function-from-inside-it where 'condition' will be 'winCondition' || 'loseCondition'. which will both default start as false. I think I should declare them false inside the game function, as opposed to having them be global variables.
-    // I am not sure how to check if the user has successfully guessed all the letters in the word. vague concept I have right now: an array that holds the correct guesses, and cycling through the string or array each time there's a correct guess to check if they have all the letters in the word in the array yet. cycle through with a foor loop, have an if statement, use a boolean, set to 'true' each time that it finds a letter in the array that matches a letter in the string, and set it to 'false' if it finds a letter in the word that's not in the array?
-    // I would need to start with a for loop equal to the string length of the word being used. then have a for loop that cycles through the entire 'correctGuess' array on each letter. and then inside THAT for loop, if it finds a letter that matches the string's letter, it sets some condition to true? but if it doesn't it sets it to false? problem: as it cycles through, it's going to inevitably find a matching letter and then a non-matching letter.
-    // is there a way to build a string containing the correct guesses in the exact same order as the actual word? and then have an if statement checking if the guessed string is exactly equal to the used word yet? and then if it is, winCondition is true and wins increments by one, and if it's not, then nothing changes?
-    // something like this, maybe: https://stackoverflow.com/questions/4364881/inserting-string-at-position-x-of-another-string/4364902 ?
-    // run through string after user guess. go until string is completed. each time the user guess matches a character in the string, fill in the character at that location with the user guess letter on screen, and also add the user guess to the userguess string at the same index as the actual word string?
-    // and then once the entire string has been run through, check if the userguess string == the word string yet.
-    // and of course, if we complete the entire string without finding the userguess matched anywhere in the string, we're going to have to subtract a guess--maybe a boolean that's default set at false, sets itself to true if we find the userguess letter in the string, and then is reset to false after the check for (did we find the guess in the string, if no, guess--), is complete?
-    // and then after we do the check for 'guess--', we check if guess is > 0, and if it's not, then the game is over and everything resets and losses increments by one.
-    // MAKE A STRING THAT IS THE UNGUESSED WORD, and then run a for loop for the word that's been picked as the word to be guessed, and make the unguessed word string an entire group of _. when the user guesses a correct letter, in the check for that, replace the _ at that index in the string with the letter the user guessed.
-    // possibly like this: https://stackoverflow.com/questions/1431094/how-do-i-replace-a-character-at-a-particular-index-in-javascript, because I'm going to have to replace the character in the unguessed word string with the letter the user guessed at the same index as the string for the word they're guessing, and then append that to the...this might not work. I'm not sure how to replace a string that's already been appended to the <div> with a new string.
-    // okay apparently you can use replace to put a different string in a div from what was already there? https://stackoverflow.com/questions/9448097/jquery-replace-a-string-inside-a-div
-    // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-} */
+function displayGuesses(guesses) {
+    document.getElementById("lives").innerHTML = "Remaining Guesses: "+guesses;
+}
+
+function displayScores(wins, losses) {
+    document.getElementById("wins").innerHTML = "Wins: "+wins;
+    document.getElementById("losses").innerHTML = "Losses: "+losses;
+}
+
+function replaceAtChar(inputString, indexToReplace, charToReplace) {
+    var startString = inputString.substring(0, indexToReplace);
+    var endString   = inputString.substring(indexToReplace + 1, inputString.length);
+    
+    return startString + charToReplace + endString;
+}
